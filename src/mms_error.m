a7f9033a81340df4adbdc703105d9df64923b480
Matthew Kehoe <mskehoe001@gmail.com>
2024-04-28 22:11:23 -0400

    Add alpha and gamma to intermediate layer calculations

--- src/mms_error.m
+++ src/mms_error.m
@@ -8,9 +8,9 @@
 
 clear all; close all;
 
-N = 16;
-M = 16;
-Nx = 32;
+N = 20;
+M = 20;
+Nx = 1024;
 N_delta = 100;
 N_Eps = 100;
 %qq = [0:5];
@@ -19,7 +19,7 @@ qq = [1];
 %N = 8; M = 8;
 
 %Eps_Max = 0.1;
-Eps_Max = 0.2;
+Eps_Max = 2.0;
 Eps = linspace(0,Eps_Max,N_Eps);
 sigma = 0.99;
 %sigma = 0.9;
@@ -38,7 +38,7 @@ r = 4;
 
 a = 4.0;
 b = 4.0;
-Nz = 32;
+Nz = 128;
 identy = eye(Nz+1);
 [Dz,z] = cheb(Nz);
 
@@ -52,11 +52,11 @@ f = (1/4)*cos(4*xx);
 f_x = -sin(4*xx);
 
 % Lipschitz Profile
-%P = 120;
-%[f,f_x] = fourier_repr_lipschitz(P,xx);
+P = 120;
+[f,f_x] = fourier_repr_lipschitz(P,xx);
 % Rough Profile
-%P = 120;
-%[f,f_x] = fourier_repr_rough(P,xx);
+% P = 120;
+% [f,f_x] = fourier_repr_rough(P,xx);
 
 % Loop over q
 
@@ -155,57 +155,53 @@ for s=1:length(qq)
       % END: DPN 6/25/21
 
       % Taylor
-      G_approx = fcn_sum(1,G_n_m,Eps(j),delta(ell),Nx,N,M);
-      J_approx = fcn_sum(1,J_n_m,Eps(j),delta(ell),Nx,N,M);
-      U_approx = fcn_sum(1,U_n_m,Eps(j),delta(ell),Nx,N,M);
-      W_approx = fcn_sum(1,W_n_m,Eps(j),delta(ell),Nx,N,M);
-      % BEGIN: DPN 6/25/21
-      ubar_approx = fcn_sum(1,ubar_n_m,Eps(j),delta(ell),Nx,N,M);
-      wbar_approx = fcn_sum(1,wbar_n_m,Eps(j),delta(ell),Nx,N,M);
-      % END: DPN 6/25/21
-      err_G_taylor(j,ell) = norm(nu_u_r-G_approx,Inf);
-      err_J_taylor(j,ell) = norm(nu_w_r-J_approx,Inf);
-      err_U_taylor(j,ell) = norm(xi_u_r-U_approx,Inf);
-      err_W_taylor(j,ell) = norm(xi_w_r-W_approx,Inf);
-      % BEGIN: DPN 6/25/21
-      err_ubar_taylor(j,ell) = norm(ubar-ubar_approx,Inf);
-      err_wbar_taylor(j,ell) = norm(wbar-wbar_approx,Inf);
-      % END: DPN 6/25/21
+      % G_approx = fcn_sum(1,G_n_m,Eps(j),delta(ell),Nx,N,M);
+      % J_approx = fcn_sum(1,J_n_m,Eps(j),delta(ell),Nx,N,M);
+      % U_approx = fcn_sum(1,U_n_m,Eps(j),delta(ell),Nx,N,M);
+      % W_approx = fcn_sum(1,W_n_m,Eps(j),delta(ell),Nx,N,M);
+      % % BEGIN: DPN 6/25/21
+      % ubar_approx = fcn_sum(1,ubar_n_m,Eps(j),delta(ell),Nx,N,M);
+      % wbar_approx = fcn_sum(1,wbar_n_m,Eps(j),delta(ell),Nx,N,M);
+      % % END: DPN 6/25/21
+      % err_G_taylor(j,ell) = norm(nu_u_r-G_approx,Inf);
+      % err_J_taylor(j,ell) = norm(nu_w_r-J_approx,Inf);
+      % err_U_taylor(j,ell) = norm(xi_u_r-U_approx,Inf);
+      % err_W_taylor(j,ell) = norm(xi_w_r-W_approx,Inf);
+      % % BEGIN: DPN 6/25/21
+      % err_ubar_taylor(j,ell) = norm(ubar-ubar_approx,Inf);
+      % err_wbar_taylor(j,ell) = norm(wbar-wbar_approx,Inf);
+      % % END: DPN 6/25/21
       
       % Pade
-      G_approx = fcn_sum(2,G_n_m,Eps(j),delta(ell),Nx,N,M);
-      J_approx = fcn_sum(2,J_n_m,Eps(j),delta(ell),Nx,N,M);
+      % G_approx = fcn_sum(2,G_n_m,Eps(j),delta(ell),Nx,N,M);
+      % J_approx = fcn_sum(2,J_n_m,Eps(j),delta(ell),Nx,N,M);
       U_approx = fcn_sum(2,U_n_m,Eps(j),delta(ell),Nx,N,M);
-      W_approx = fcn_sum(2,W_n_m,Eps(j),delta(ell),Nx,N,M);
-      % BEGIN: DPN 6/25/21
+      % W_approx = fcn_sum(2,W_n_m,Eps(j),delta(ell),Nx,N,M);
       ubar_approx = fcn_sum(2,ubar_n_m,Eps(j),delta(ell),Nx,N,M);
-      wbar_approx = fcn_sum(2,wbar_n_m,Eps(j),delta(ell),Nx,N,M);
-      % END: DPN 6/25/21
-      err_G_pade(j,ell) = norm(nu_u_r-G_approx,Inf);
-      err_J_pade(j,ell) = norm(nu_w_r-J_approx,Inf);
+      % wbar_approx = fcn_sum(2,wbar_n_m,Eps(j),delta(ell),Nx,N,M);
+      % err_G_pade(j,ell) = norm(nu_u_r-G_approx,Inf);
+      % err_J_pade(j,ell) = norm(nu_w_r-J_approx,Inf);
       err_U_pade(j,ell) = norm(xi_u_r-U_approx,Inf);
-      err_W_pade(j,ell) = norm(xi_w_r-W_approx,Inf);
-      % BEGIN: DPN 6/25/21
+      % err_W_pade(j,ell) = norm(xi_w_r-W_approx,Inf);
       err_ubar_pade(j,ell) = norm(ubar-ubar_approx,Inf);
-      err_wbar_pade(j,ell) = norm(wbar-wbar_approx,Inf);
-      % END: DPN 6/25/21
+      % err_wbar_pade(j,ell) = norm(wbar-wbar_approx,Inf);
       
       % Pade (safe)
-      G_approx = fcn_sum(3,G_n_m,Eps(j),delta(ell),Nx,N,M);
-      J_approx = fcn_sum(3,J_n_m,Eps(j),delta(ell),Nx,N,M);
-      U_approx = fcn_sum(3,U_n_m,Eps(j),delta(ell),Nx,N,M);
-      W_approx = fcn_sum(3,W_n_m,Eps(j),delta(ell),Nx,N,M);
+      % G_approx = fcn_sum(3,G_n_m,Eps(j),delta(ell),Nx,N,M);
+      % J_approx = fcn_sum(3,J_n_m,Eps(j),delta(ell),Nx,N,M);
+      % U_approx = fcn_sum(3,U_n_m,Eps(j),delta(ell),Nx,N,M);
+      % W_approx = fcn_sum(3,W_n_m,Eps(j),delta(ell),Nx,N,M);
       % BEGIN: DPN 6/25/21
-      ubar_approx = fcn_sum(3,ubar_n_m,Eps(j),delta(ell),Nx,N,M);
-      wbar_approx = fcn_sum(3,wbar_n_m,Eps(j),delta(ell),Nx,N,M);
+      % ubar_approx = fcn_sum(3,ubar_n_m,Eps(j),delta(ell),Nx,N,M);
+      % wbar_approx = fcn_sum(3,wbar_n_m,Eps(j),delta(ell),Nx,N,M);
       % END: DPN 6/25/21
-      err_G_pade_safe(j,ell) = norm(nu_u_r-G_approx,Inf);
-      err_J_pade_safe(j,ell) = norm(nu_w_r-J_approx,Inf);
-      err_U_pade_safe(j,ell) = norm(xi_u_r-U_approx,Inf);
-      err_W_pade_safe(j,ell) = norm(xi_w_r-W_approx,Inf);
+      % err_G_pade_safe(j,ell) = norm(nu_u_r-G_approx,Inf);
+      % err_J_pade_safe(j,ell) = norm(nu_w_r-J_approx,Inf);
+      % err_U_pade_safe(j,ell) = norm(xi_u_r-U_approx,Inf);
+      % err_W_pade_safe(j,ell) = norm(xi_w_r-W_approx,Inf);
       % BEGIN: DPN 6/25/21
-      err_ubar_pade_safe(j,ell) = norm(ubar-ubar_approx,Inf);
-      err_wbar_pade_safe(j,ell) = norm(wbar-wbar_approx,Inf);
+      % err_ubar_pade_safe(j,ell) = norm(ubar-ubar_approx,Inf);
+      % err_wbar_pade_safe(j,ell) = norm(wbar-wbar_approx,Inf);
       % END: DPN 6/25/21
     end
   end
